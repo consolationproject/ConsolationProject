aa9859d50f74d093f2e7b7a7fbee929ee4aa4fe7
[0202zc] <[lzc980202@163.com]>
2019-04-12 23:28:50 +0800

    修复DAOFactory1的问题

--- ConsolationProject/src/dao/DAOFactory1.java
+++ ConsolationProject/src/dao/DAOFactory1.java
@@ -1,5 +1,7 @@
 package dao;
 
+import dao.impl.MoodRecordDAOImpl;
+import dao.impl.SongDAOImpl;
 import dao.impl.UserDAOImpl;
 
 public class DAOFactory1 extends DAOFactory {
@@ -8,4 +10,16 @@ public class DAOFactory1 extends DAOFactory {
     public UserDAO getUserDAO() {
         return new UserDAOImpl();
     }
+
+    @Override
+    public SongDAO getSongDAO() {
+        return new SongDAOImpl();
+    }
+
+    @Override
+    public MoodRecordDAO getMoodRecordDAO() {
+        return new MoodRecordDAOImpl();
+    }
+
+
 }
